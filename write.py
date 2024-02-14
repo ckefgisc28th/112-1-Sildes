04a3a76b52b93fae2452fafc2fc4040addaa2c95
menghsuan <menghsuanyang@gmail.com>
2024-02-15 03:15:38 +0800

    update all

--- write.py
+++ write.py
@@ -1,18 +1,18 @@
 import os
 
-now = "Blender"
+now = "Python"
 
 f = open(f"./{now}/{now}.txt",mode="r")
 urls = f.readlines()
 f.close()
 print(urls[0])
 
-for i in range(1,15):
-    f = open(f"./{now}/Main-{i}.html",mode="w")
+for i in range(0,len(urls)):
+    f = open(f"./{now}/{now}-{i+1}.html",mode="w")
     f.write("<!DOCTYPE html>\n")
     f.write('<html lang="en">\n')
     f.write("<head>\n")
-    f.write('    <meta http-equiv="refresh" content="0;url='+urls[i-1].split("\n")[0]+'"/>\n')
+    f.write('    <meta http-equiv="refresh" content="0;url='+urls[i].split("\n")[0]+'"/>\n')
     f.write("</head>\n")
     f.write("</html>")
     f.close()
